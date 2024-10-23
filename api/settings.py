a075d2ac347deb379096716fe9ee10cb0c660368
Louiexz <fluizlucas@gmail.com>
2024-10-23 10:02:15 -0300

    Deploy no render

--- api/settings.py
+++ api/settings.py
@@ -40,6 +40,7 @@ LOGIN_URL = '/sign-in/'
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SESSION_COOKIE_AGE = 1800
 
